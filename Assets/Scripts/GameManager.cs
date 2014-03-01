e463b6f29d6aed17879823079c423d36adad0cf5
Donna Lou <ambivalenc3@gmail.com>
2014-02-28 17:35:03 -0800

    Add background and wall box colliders
    background & colliders

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Camera mainCam;
+	public BoxCollider2D WallBottom;
+	public BoxCollider2D WallRight;
+	public BoxCollider2D WallTop;
+	public BoxCollider2D WallLeft;
+	
+	
+	void Start () {
+		WallTop.size = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
+		WallTop.center = new Vector2(0f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y + 0.5f);
+		
+		WallBottom.size = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
+		WallBottom.center = new Vector2(0f, mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y - 0.5f);
+		
+		WallLeft.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y);
+		WallLeft.center = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x, 0f);
+		
+		WallRight.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3( 0f, Screen.height * 2f, 0f)).y);
+		WallRight.center = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x, 0f);
+	}
+}
